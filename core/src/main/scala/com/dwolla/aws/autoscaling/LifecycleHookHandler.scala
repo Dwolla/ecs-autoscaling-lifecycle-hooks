3f6ee3f1e7470706c4fa79d60cabba3e20c589de
Brian Holt <bholt@dwolla.com>
2023-12-06 16:49:53 -0600

    add more detailed tracing logs in the hope that knowing the trace IDs will help us find the traces in X-Ray

--- core/src/main/scala/com/dwolla/aws/autoscaling/LifecycleHookHandler.scala
+++ core/src/main/scala/com/dwolla/aws/autoscaling/LifecycleHookHandler.scala
@@ -9,7 +9,8 @@ import com.dwolla.aws.sns.*
 import feral.lambda.events.SnsEvent
 import feral.lambda.{INothing, LambdaEnv}
 import fs2.Stream
-import natchez.{EntryPoint, Span}
+import natchez.{EntryPoint, Span, Trace}
+import natchez.mtl.given
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 import com.dwolla.tracing.syntax.*
 
@@ -18,15 +19,19 @@ object LifecycleHookHandler {
                                                              (eventBridge: (TopicARN, LifecycleHookNotification) => F[Unit])
                                                              (using fs2.Compiler[F, F], Local[F, Span[F]]): LambdaEnv[F, SnsEvent] => F[Option[INothing]] = env =>
     entryPoint.runInRoot(hookName) {
-      Stream.eval(env.event)
-        .map(_.records)
-        .flatMap(Stream.emits(_))
-        .map(_.sns)
-        .evalMap(ParseLifecycleHookNotification[F])
-        .unNone
-        .evalMap(eventBridge.tupled)
-        .compile
-        .drain
-        .as(None)
+      Trace[F].kernel.flatMap(k => Logger[F].info(s"trace kernel: ${k.toHeaders}")) >>
+        Stream.eval(env.event)
+          .map(_.records)
+          .flatMap(Stream.emits(_))
+          .map(_.sns)
+          .evalMap(ParseLifecycleHookNotification[F])
+          .unNone
+          .evalMap(eventBridge.tupled)
+          .compile
+          .drain
+          .as(None)
     }
+      .flatTap { _ =>
+        Logger[F].info("tracing should be complete by now")
+      }
 }
